09505685f5ef5de74741c47cabd4d0e0b3b16544
Richard Twena <Richardtwena@gmail.com>
2015-02-17 10:51:54 +0000

    string vs int check

--- sort_exercise2.rb
+++ sort_exercise2.rb
@@ -1,7 +1,43 @@
-array = 6,5,2,4,3,1
+require 'pry'
+array = [6, 5, 2, 4, 3, 1, 'a', 'c', 'b']
 
-def sort(array, i)
+def sort(array, rightside, leftside)
+	array.length.times do
+		if array[rightside].class == Fixnum && array[leftside].class == Fixnum
+			if array[rightside] < array[leftside]
+				temp = array[leftside]
+				array[leftside] = array[rightside]
+				array[rightside] = temp
 
+				sort(array, rightside+1, leftside+1) if array.length != rightside+1			
+			elsif array.length != rightside+1
+				sort(array, rightside+1, leftside+1)
+			end
+		end
+
+		if array[rightside].class == String && array[leftside].class == String
+			puts 'hi'
+			if array[rightside] < array[leftside]
+				temp = array[leftside]
+				array[leftside] = array[rightside]
+				array[rightside] = temp
+
+				sort(array, rightside+1, leftside+1) if array.length != rightside+1			
+			elsif array.length != rightside+1
+				sort(array, rightside+1, leftside+1)
+			end
+		end
+		sort(array, rightside+1, leftside+1) if array.length < rightside+1
+	end
 end
 
-sort(array, 0)
+sort(array, 1, 0)	
+
+puts array
+
+# if array[rightside].class == String && array[leftside].class == String
+# 					if array[rightside] < array[leftside]
+# 					temp = array[leftside]
+# 					array[leftside] = array[rightside]
+# 					array[rightside] = temp
+# 				end
