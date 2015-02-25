c4f5f9b52c72f1ed4b0097c1f05f0e02976d4e5e
Richard Twena <Richardtwena@gmail.com>
2015-02-25 08:43:48 +0000

    removed tabs for spaces, abstracted swap from sort method, more dryness added

--- sort_exercise2.rb
+++ sort_exercise2.rb
@@ -1,26 +1,27 @@
-array = [6, 5, 'b', 1, 2, 4, 3, 1, 'a']
+array = [6, 5, 'b', 1, 2, 4, 3, 1, 'a', 2]
 str_array = []
 
 def split_array (array, str_array)
-	array.each_with_index{|value, index|
-		if value.class == String
-			str_array << array.delete_at(index)
-		end
-	}
+  array.each_with_index{|value, index|
+    str_array << array.delete_at(index) if value.class == String
+  }
 end
 
 def sort(array, rightside, leftside)
-	array.length.times do
-		if array[rightside] < array[leftside]
-			temp = array[leftside]
-			array[leftside] = array[rightside]
-			array[rightside] = temp
+  array.length.times do
+    if array[rightside] < array[leftside]
+      swap(array, rightside, leftside)
+      sort(array, rightside+1, leftside+1) if array.length != rightside+1
+    elsif array.length != rightside+1
+      sort(array, rightside+1, leftside+1)
+    end
+  end
+end
 
-			sort(array, rightside+1, leftside+1) if array.length != rightside+1			
-		elsif array.length != rightside+1
-			sort(array, rightside+1, leftside+1)
-		end
-	end
+def swap(array, rightside, leftside)
+  temp = array[leftside]
+  array[leftside] = array[rightside]
+  array[rightside] = temp
 end
 
 split_array(array, str_array)
@@ -29,4 +30,4 @@ sort(array, 1, 0)
 sort(str_array, 1, 0)	
 
 array << str_array
-puts array
+puts array 
